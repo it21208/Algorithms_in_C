a692ba9e48934c53ba170c71a689332875277ba0
Alexandros Ioannidis <alexioanid@yahoo.gr>
2021-04-23 21:41:56 +0300

    Update sequentialSearch.c
    this function checks if the number v is included in an already stored domain of numbers in the objects a[1], a[l+1], ..., a[r] of the table by comparing the v number with each number sequentially from the beginning. If we reach the end without finding the wanted number we return the value -1. Alternatively, we return the numerator of the position in the one-dimensional array that contains this number.

--- sequentialSearch.c
+++ sequentialSearch.c
@@ -1,1 +1,12 @@
+/* this function checks if  the number v is included in an already stored domain of numbers in the objects a[1], a[l+1], ..., a[r] of the table
+    by comparing the v number with each number sequentially from the beginning. If we reach the end without finding the wanted number we return the value -1.
+    Alternatively, we return the numerator of the position in the one dimensional array that contains this number.
+*/
 
+int search(int a[], int v, int l, int r)
+    {
+      int i; 
+      for (i = 1; i <= r; i++)
+        if (v == a[i]) return i;
+      return -1;
+    }
