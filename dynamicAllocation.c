e35933401b7f886e0ef4a19b907576d31f450bf0
Alexandros Ioannidis <alexioanid@yahoo.gr>
2021-04-24 11:43:26 +0300

    Create dynamicAllocation.c

---
+++ dynamicAllocation.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+
+main(int argc, char *argv[])
+{
+  long int i, j, N = atoi(argv[1]);
+  int *a = malloc(N*sizeof(int));
+  if (a == NULL)
+  {
+    printf("Insufficient memory.\n");
+  }
+ ...
